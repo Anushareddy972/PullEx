db5e56267c55170c06a019028d6e33ef3f8780a0
Anushareddy972 <75823700+Anushareddy972@users.noreply.github.com>
2023-01-30 12:03:35 -0600

    Update AddingNumbers.java
    added code

--- src/main/java/com/pullreq/examples/AddingNumbers.java
+++ src/main/java/com/pullreq/examples/AddingNumbers.java
@@ -1,2 +1,10 @@
-package com.pullreq.examples;public class AddingNumbers {
+package com.pullreq.examples;
+public class AddingNumbers {
+  public static void main(String[] args)
+  {
+    int a=20;
+    int b=30;
+    System.out.println("addition of two numbers:"+(a+b));
+  }
+   
 }
