92ea891b6ee84e2c32a09901783fad8eee222205
Anushareddy972 <75823700+Anushareddy972@users.noreply.github.com>
2023-01-30 12:08:03 -0600

    Update Tables.java
    Added table code

--- src/main/java/com/pullreq/examples/Tables.java
+++ src/main/java/com/pullreq/examples/Tables.java
@@ -1,2 +1,16 @@
-package com.pullreq.examples;public class Tables {
+package com.pullreq.examples;
+import java.util.Scanner;
+public class Tables {
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number you want to multiply:");
+    int n = sc.nextInt();
+    for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+    
+  }
+  
 }
